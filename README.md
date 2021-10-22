7a5f9c0d0e6b6b45707fdc0dc9832a26f6a0442a
Luk4x <luk4xm4ci3l@gmail.com>
2021-10-22 08:41:42 -0300

    a4

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # nlw-Heat-Origin-
-Evento da Rocketseat relacionado a desenvolvimento web (html, css e javascript).
+
+Evento da Rocketseat (2021) relacionado a desenvolvimento web (html, css e javascript).
---
+++ main.js
@@ -0,0 +1,28 @@
+const linksSocialMedia = {
+  github: 'Luk4x',
+  youtube: 'channel/UCKIAhjkmwUSk6fJ-4OXZklg',
+  instagram: 'lu_k4x'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+getGitHubProfileInfo()
+
+function getGitHubProfileInfo() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+  fetch(url)
+    .then(reponse => reponse.json())
+    .then(data => {
+      gitHubName.textContent = data.name
+      gitHubBio.textContent = data.bio
+      gitHubPhoto.src = data.avatar_url
+      gitHubUrl.href = data.html_url
+      gitHubLogin.textContent = data.login
+    })
+}
